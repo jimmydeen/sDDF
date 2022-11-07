d2de1a3175bad473e39ad6060e5839b55fe827d2
Krishnan Winter <z5258205@ad.unsw.edu.au>
2022-11-07 14:17:36 +1100

    Copied over init functions from seL4 util_libs

--- echo_server/serial.c
+++ echo_server/serial.c
@@ -6,7 +6,7 @@
 #include <stdint.h>
 #include <sel4cp.h>
 #include <sel4/sel4.h>
-#include "eth.h"
+#include "serial.h"
 #include "shared_ringbuffer.h"
 
 // Defines to manage interrupts and notifications
@@ -32,4 +32,99 @@ ring_handle_t tx_ring;
 
 uintptr_t serial_regs;
 
+/*
+ * BaudRate = RefFreq / (16 * (BMR + 1)/(BIR + 1) )
+ * BMR and BIR are 16 bit
+ * Function taken from seL4 util_libs serial.c implementation for imx8mm
+ */
+static void imx_uart_set_baud(long bps)
+{
+    imx_uart_regs_t *regs = &uart_base;
+    uint32_t bmr, bir, fcr;
+    fcr = regs->fcr;
+    fcr &= ~UART_FCR_RFDIV_MASK;
+    fcr |= UART_FCR_RFDIV(4);
+    bir = 0xf;
+    bmr = UART_REF_CLK / bps - 1;
+    regs->bir = bir;
+    regs->bmr = bmr;
+    regs->fcr = fcr;
+}
 
+int serial_configure(
+    long bps,
+    int char_size,
+    enum serial_parity parity,
+    int stop_bits)
+{
+    imx_uart_regs_t *regs = &uart_base;
+    uint32_t cr2;
+    /* Character size */
+    cr2 = regs->cr2;
+    if (char_size == 8) {
+        cr2 |= UART_CR2_WS;
+    } else if (char_size == 7) {
+        cr2 &= ~UART_CR2_WS;
+    } else {
+        return -1;
+    }
+    /* Stop bits */
+    if (stop_bits == 2) {
+        cr2 |= UART_CR2_STPB;
+    } else if (stop_bits == 1) {
+        cr2 &= ~UART_CR2_STPB;
+    } else {
+        return -1;
+    }
+    /* Parity */
+    if (parity == PARITY_NONE) {
+        cr2 &= ~UART_CR2_PREN;
+    } else if (parity == PARITY_ODD) {
+        /* ODD */
+        cr2 |= UART_CR2_PREN;
+        cr2 |= UART_CR2_PROE;
+    } else if (parity == PARITY_EVEN) {
+        /* Even */
+        cr2 |= UART_CR2_PREN;
+        cr2 &= ~UART_CR2_PROE;
+    } else {
+        return -1;
+    }
+    /* Apply the changes */
+    regs->cr2 = cr2;
+    /* Now set the board rate */
+    imx_uart_set_bau(bps);
+    return 0;
+}
+
+// Init function required by CP for every PD
+void init(void) {
+    sel4cp_dbg_puts(sel4cp_name);
+    sel4cp_dbg_puts(": elf PD init function running\n");
+
+    imx_uart_regs_t *regs = &uart_base;
+
+    /* Software reset */
+    regs->cr2 &= ~UART_CR2_SRST;
+    while (!(regs->cr2 & UART_CR2_SRST));
+
+    /* Line configuration */
+    serial_configure(115200, 8, PARITY_NONE, 1);
+
+    /* Enable the UART */
+    regs->cr1 |= UART_CR1_UARTEN;                /* Enable The uart.                  */
+    regs->cr2 |= UART_CR2_RXEN | UART_CR2_TXEN;  /* RX/TX enable                      */
+    regs->cr2 |= UART_CR2_IRTS;                  /* Ignore RTS                        */
+    regs->cr3 |= UART_CR3_RXDMUXDEL;             /* Configure the RX MUX              */
+    /* Initialise the receiver interrupt.                                             */
+    regs->cr1 &= ~UART_CR1_RRDYEN;               /* Disable recv interrupt.           */
+    regs->fcr &= ~UART_FCR_RXTL_MASK;            /* Clear the rx trigger level value. */
+    regs->fcr |= UART_FCR_RXTL(1);               /* Set the rx tigger level to 1.     */
+    regs->cr1 |= UART_CR1_RRDYEN;                /* Enable recv interrupt.            */
+}
+
+// Entry point that is invoked on a serial interrupt
+void notified(sel4cp_channel ch) {
+    sel4cp_dbg_puts(sel4cp_name);
+    sel4cp_dbg_puts(": elf PD notified function running\n");
+}
