342e3c615028232ef61e3d1ca60e24ee0a9884d0
Krishnan Winter <krishnanwinter1@gmail.com>
2023-01-16 13:49:23 +1100

    Added in get char implementation

--- echo_server/serial_server.c
+++ echo_server/serial_server.c
@@ -74,6 +74,51 @@ int serial_server_printf(char *string) {
         sel4cp_notify(SERVER_PRINTF_CHANNEL);
     }
 
+    return 0;
+}
+
+// Return char on success, -1 on failure
+int getchar() {
+    // Notify the driver that we want to get a character. In Patrick's design, this increments 
+    // the chars_for_clients value.
+    sel4cp_notify(SERVER_GETCHAR_CHANNEL);
+
+    struct serial_server *local_server = &global_serial_server;
+
+    /* Now that we have notified the driver, we can attempt to dequeue from the used ring.
+    When the driver has processed an interrupt, it will add the inputted character to the used ring.*/
+    
+    // Address that we will pass to dequeue to store the buffer address
+    uintptr_t buffer_addr;
+    // Integer to store the length of the buffer
+    int buffer_len; 
+
+
+    while (dequeue_used(&local_server->rx_ring, &buffer_addr, &buffer_len, NULL) != 0) {
+        /* The ring is currently empty, so there is no character to get. 
+        We will spin here until we have gotten a character. As the driver is a higher priority than us, 
+        it should be able to pre-empt this loop
+        */
+
+    }
+
+    // We are only getting one character at a time, so we just need to cast the buffer to an int
+
+    char got_char = *((char *) buffer_addr);
+
+    // Clear the buffer
+    *buffer_addr = 0;
+
+    /* Now that we are finished with the used buffer, we can add it back to the available ring*/
+
+    int ret = enqueue_avail(&local_server->rx_ring, &buffer_addr, &buffer_len, NULL);
+
+    if (ret != 0) {
+         sel4cp_dbg_puts(sel4cp_name);
+        sel4cp_dbg_puts(": getchar - unable to enqueue used buffer back into available ring\n");
+    }
+
+    return (int) got_char;
 }
 
 // Init function required by sel4cp, initialise serial datastructres for server here
